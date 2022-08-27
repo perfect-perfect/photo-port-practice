f6f6aabde1a7d7f07a7836327e4046346467709c
perfect perfect <gilberto.j.covarrubias@gmail.com>
2022-08-27 18:17:55 -0400

    finished image modal

---
+++ src/components/Modal/index.js
@@ -0,0 +1,24 @@
+function Modal({onClose, currentPhoto}){
+    // destructure the currentPhoto 'props'
+    const {name, category, description, index} = currentPhoto;
+
+    return (
+        <div className="modalBackdrop">
+            <div className="modalContainer">
+                <h3 className="modalTitle">{name}</h3>
+                <img 
+                    src={require(`../../assets/large/${category}/${index}.jpg`)}
+                    alt="current category" 
+                />
+                <p>
+                    {description}
+                </p>
+                <button onClick={onClose} type="button">
+                    Close this modal
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import Modal from "../Modal";
 
 // alternatiely we could just call it 'props' and then use 'props.category' (it is .category, because we named the prop 'category' when we passed it down in the Gallery component)
 // here we are using destructuring to rename the prop 'category'
 function PhotoList({ category }) {
+    // we use the 'useState()' Hook to manage the current photo state and make this data accesible to the 'Modal' component through props
+    const [currentPhoto, setCurrentPhoto] = useState();
+    // we set the initial state to 'false' because we don't want the modal to open until a user has clicked on an image
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const [photos] = useState([
         {
@@ -102,12 +107,24 @@ function PhotoList({ category }) {
         },
     ]);
 
+    // each object in the photo array was named 'image' in the currentPhotos.map()
+    const toggleModal = (image, i) => {
+        // we updated the current photo state using the 'setCurrentPhoto' function with the data retrieved through the click event
+        setCurrentPhoto({...image, index: i})
+        // when an image is clicked toggleModal is called and it sets isModalOpen to 'false'
+        setIsModalOpen(!isModalOpen);
+    }
     // we're going through each 'photo' in the 'photos' array, trying to find every photo that matches the 'category'.
     //  - if the photo matches the condition, it is returned in an array and assigned to 'currentPhotos'
     const currentPhotos = photos.filter((photo) => photo.category === category);
 
     return (
         <div>
+            {/* The Modal will only render if 'isModalOpen' is true */}
+            {isModalOpen && (
+                // onClose identifier was assigned the function 'toggleModal'
+                <Modal currentPhoto={currentPhoto} onClose={toggleModal}/>
+            )}
             <div className="flex-row">
                 {/* we map the 'currentPhotos' array we created to display each photo that matches our currently selected category */}
                 {/* the 'i' is interesting here. it is very crucial. it is determined by the index number of photo in the filtered and created currentPhotos. That means the photos must have names that correspond to what that 'i' index will be. This means that the photos must be place in src/assets/small/commercial in the order that they will be in currentPhotos. since [photos] created currentPhotos. then photos must also be placed ina way that will result in the index 'i' being the same name of the photo (i sort of rambled here sorry) */}
@@ -116,6 +133,7 @@ function PhotoList({ category }) {
                         src={require(`../../assets/small/${category}/${i}.jpg`)}
                         alt={image.name}
                         className="img-thumbnail mx-1"
+                        onClick={() => toggleModal(image, i)}
                         key={image.name}
                     />
                 ))}
